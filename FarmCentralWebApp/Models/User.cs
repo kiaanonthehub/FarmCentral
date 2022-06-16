6b4a7ccbbeeb31371a710d7ec053c8bff2968eee
Kiaan Maharaj <kiaanmaharaj7@gmail.com>
2022-06-16 04:09:14 +0200

    Login View | Data Validation | Tested

--- FarmCentralWebApp/Models/User.cs
+++ FarmCentralWebApp/Models/User.cs
@@ -6,10 +6,15 @@ namespace FarmCentralWebApp.Models
     {
         [Key]
         public int UserId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Surname { get; set; }
+        [Required] 
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string Role { get; set; }
     }
 }
--- FarmCentralWebApp/ViewModels/Login.cs
+++ FarmCentralWebApp/ViewModels/Login.cs
@@ -1,9 +1,15 @@
-﻿namespace FarmCentralWebApp.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FarmCentralWebApp.ViewModels
 {
     public class Login
     {
+        [Required(ErrorMessage = "A valid email address is required")]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required(ErrorMessage = "A valid password for this account is required to log in")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "A valid role is required")]
         public string Role { get; set; }
     }
 }
